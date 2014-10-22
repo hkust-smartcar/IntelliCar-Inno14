32b7db3f59d1e18d45cc9aad16eac5b012495462
Ming Ming <mint2648@gmail.com>
2014-10-22 18:09:32 +0800

    Add initial car

---
+++ src/car.cpp
@@ -0,0 +1,59 @@
+/*
+ * car.cpp
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ */
+
+#include <cstddef>
+#include <memory>
+
+#include <libsc/k60/encoder.h>
+#include <libsc/k60/led.h>
+#include <libsc/k60/motor.h>
+#include <libsc/k60/simple_buzzer.h>
+#include <libsc/k60/trs_d05.h>
+#include <libsc/k60/uart_device.h>
+#include <libutil/misc.h>
+#include <libutil/remote_var_manager.h>
+
+#include "car.h"
+
+using namespace libsc::k60;
+using namespace std;
+
+#define SERVO_MID_DEGREE 815
+#define SERVO_AMPLITUDE 290
+
+namespace inno
+{
+
+Car::Car()
+		: m_encoder(0),
+		  m_leds{Led(0), Led(1), Led(2), Led(3)},
+		  m_motor(0, false),
+		  m_buzzer(0),
+		  m_servo(0),
+		  m_uart(0, libbase::k60::Uart::Config::BaudRate::k115200)
+{}
+
+Car::~Car()
+{}
+
+void Car::SetMotorPower(const int16_t power)
+{
+	const Uint power_ = libutil::Clamp<Uint>(0, abs(power), 1000);
+	m_motor.SetClockwise(power < 0);
+	m_motor.SetPower(power_);
+}
+
+void Car::SetTurning(const int16_t percentage)
+{
+	// Servo's rotation dir is opposite to our wheels
+	const int percentage_ = libutil::Clamp<int>(-100, -percentage, 100);
+	const int degree = SERVO_MID_DEGREE + (percentage_ * SERVO_AMPLITUDE / 1000);
+	m_servo.SetDegree(degree);
+}
+
+}
---
+++ src/car.h
@@ -0,0 +1,82 @@
+/*
+ * car.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <cstddef>
+
+#include <libsc/k60/encoder.h>
+#include <libsc/k60/led.h>
+#include <libsc/k60/motor.h>
+#include <libsc/k60/simple_buzzer.h>
+#include <libsc/k60/trs_d05.h>
+#include <libsc/k60/uart_device.h>
+
+namespace inno
+{
+
+class Car
+{
+public:
+	Car();
+	~Car();
+
+	/**
+	 * Set the power of the motor, a negative power will drive the car backwards
+	 *
+	 * @param power Power scale in [-1000, 1000]
+	 */
+	void SetMotorPower(const int16_t power);
+
+	/**
+	 * Set the turning percentage, negative input means turning left
+	 *
+	 * @param percentage Specifying how aggressively should the car turn,
+	 * in [-1000, 1000], where passing 0 basically means going straight
+	 */
+	void SetTurning(const int16_t percentage);
+
+	void SwitchLed(const uint8_t id)
+	{
+		m_leds[id].Switch();
+	}
+	void SetLed(const uint8_t id, const bool flag)
+	{
+		m_leds[id].SetEnable(flag);
+	}
+
+	void UpdateEncoder()
+	{
+		m_encoder.Update();
+	}
+
+	bool UartPeekChar(char *out_ch)
+	{
+		return m_uart.PeekChar(out_ch);
+	}
+
+	int16_t GetEncoderCount()
+	{
+		return m_encoder.GetCount();
+	}
+
+	void EnableUartRx(const libsc::k60::UartDevice::OnReceiveListener &l)
+	{
+		m_uart.EnableRx(l);
+	}
+
+private:
+	libsc::k60::Encoder m_encoder;
+	libsc::k60::Led m_leds[4];
+	libsc::k60::Motor m_motor;
+	libsc::k60::SimpleBuzzer m_buzzer;
+	libsc::k60::TrsD05 m_servo;
+	libsc::k60::UartDevice m_uart;
+};
+
+}
