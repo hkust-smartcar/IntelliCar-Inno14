08cf36c5b412e1f11e45361016e89d3b0021bdea
Ming Ming <mint2648@gmail.com>
2014-12-25 01:15:09 +0800

    Add MCG config

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,22 @@
 
 using namespace inno;
 
+namespace libbase
+{
+namespace k60
+{
+
+Mcg::Config Mcg::GetMcgConfig()
+{
+	Mcg::Config config;
+	config.external_oscillator_khz = 50000;
+	config.core_clock_khz = 150000;
+	return config;
+}
+
+}
+}
+
 int main()
 {
 	App app;
